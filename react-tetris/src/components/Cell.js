7fa0208bc81da51849845c7c3ad0357c37a2906e
rtjae1 <ryan.t.jae@gmail.com>
2019-08-16 19:37:45 -0400

    figured background out

--- react-tetris/src/components/Cell.js
+++ react-tetris/src/components/Cell.js
@@ -1,5 +1,11 @@
 import React from "react";
+import { StyledCell } from "./styles/StyledCell";
+import { TETROMINOS } from "../tetrominos";
 
-const Cell = ({ type }) => <div>cell</div>;
+const Cell = ({ type }) => (
+  <StyledCell type={"L"} color={TETROMINOS["L"].color}>
+    cell
+  </StyledCell>
+);
 
 export default Cell;
